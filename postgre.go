0d0fe78df864b132621d6a6f34698e485288a3bd
TARUN-KUMAR-RAJPUT <71704522+TARUN-KUMAR-RAJPUT@users.noreply.github.com>
2023-04-29 17:59:09 +0530

    Update postgre.go

--- postgre.go
+++ postgre.go
@@ -12,7 +12,7 @@ import (
 type User struct {
     ID   int    `json:"id"`
     Name string `json:"name"`
-	Mobile string `json:"mobile"`
+    Mobile string `json:"mobile"`
 }
 
 func main() {
@@ -32,56 +32,56 @@ func main() {
 
     // Define a GET route to retrieve all users from the "users" table
     r.GET("/users", func(c *gin.Context) {
-		
-		// Perform a query
-		rows, err := db.Query("select * from test_table_uchiha")
-		if err != nil {
-			panic(err)
-		}
-		defer rows.Close()
 
-        fmt.Println(rows)
-        // Create a slice of User structs to store the retrieved users
-        var users []User
+	// Perform a query
+	rows, err := db.Query("select * from test_table_uchiha")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
 
-        // Loop through the result set and create a User struct for each row
-        for rows.Next() {
-            var user User
-            err := rows.Scan(&user.ID, &user.Name, &user.Mobile)
-            if err != nil {
-                fmt.Println(err)
-            }
-            users = append(users, user)
-        }
+	fmt.Println(rows)
+	// Create a slice of User structs to store the retrieved users
+	var users []User
 
-        // Check for any errors that may have occurred while iterating through the result set
-        err = rows.Err()
-        if err != nil {
+	// Loop through the result set and create a User struct for each row
+	for rows.Next() {
+	    var user User
+	    err := rows.Scan(&user.ID, &user.Name, &user.Mobile)
+	    if err != nil {
+		fmt.Println(err)
+	    }
+	    users = append(users, user)
+	}
+
+	// Check for any errors that may have occurred while iterating through the result set
+	err = rows.Err()
+	if err != nil {
 			fmt.Println(err)
-        }
+	}
 
-        // Return the slice of User structs as JSON
-        c.JSON(200, users)
+	// Return the slice of User structs as JSON
+	c.JSON(200, users)
     })
 
 	r.POST("/addusers", func(c *gin.Context) {
-        // Bind the request body to a User struct
-        var user User
-        err := c.BindJSON(&user)
-        if err != nil {
+	// Bind the request body to a User struct
+	var user User
+	err := c.BindJSON(&user)
+	if err != nil {
 		fmt.Println(err)
-        }
+	}
 
-        // Execute an INSERT query to insert the new user into the "users" table
-        _, err = db.Exec("INSERT INTO test_table_uchiha (id, name, mobile) VALUES ($1, $2, $3)", user.ID, user.Name, user.Mobile)
-        if err != nil {
+	// Execute an INSERT query to insert the new user into the "users" table
+	_, err = db.Exec("INSERT INTO test_table_uchiha (id, name, mobile) VALUES ($1, $2, $3)", user.ID, user.Name, user.Mobile)
+	if err != nil {
 		fmt.Println(err)
-        }
+	}
 
-        // Return a success message
-        c.JSON(200, gin.H{
-            "message": "User created",
-        })
+	// Return a success message
+	c.JSON(200, gin.H{
+	    "message": "User created",
+	})
     })
 
 
